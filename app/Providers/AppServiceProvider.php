2f5edc42f1912d51b6fe809bb54f02603a5cb074
kamaleshvr <kamaleshvr0517@gmail.com>
2026-02-25 00:25:52 +0530

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+         if (env('APP_ENV') === 'production') {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
+         }
         try {
             // Share theme with all views using View::share
             if (\Illuminate\Support\Facades\Schema::hasTable('settings')) {
